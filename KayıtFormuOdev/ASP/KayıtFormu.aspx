3e7c56fe2dcf8f86b40e08909e41ac32461e0538
bberksimsekk <simsekberk11@gmail.com>
2024-08-25 06:06:10 +0300

    Dosyayı Ekleyip Yaptım

---
+++ KayıtFormuOdev/ASP/KayıtFormu.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="KayıtFormu.aspx.cs" Inherits="KayıtFormuOdev.ASP_Kontrolleri.KayıtFormu" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <link href="CSS/KayıtFormuStyle.css" rel="stylesheet" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div class="KayıtPanel">
+            <div class="satir" style="text-align:center">
+                <img src="Images/ryan-reynolds-hugh-jackman.gif" width="300" />
+            </div>
+        <div class="Satır">
+            Kullanıcı Adı: <br />
+            <asp:TextBox ID="tb_kullanıcı" runat="server" CssClass="MetinKutu" PlaceHolder="Kullanıcı Adı Giriniz!"></asp:TextBox>
+        </div>
+        <div class="Satır">
+            Email: <br />
+            <asp:TextBox ID="tb_email" runat="server" CssClass="MetinKutu" PlaceHolder="Email Giriniz!"></asp:TextBox>
+        </div>
+        <div class="Satır">
+            Şifre: <br />
+            <asp:TextBox ID="tb_sifre" runat="server" CssClass="MetinKutu" PlaceHolder="Şifreyi giriniz!"></asp:TextBox>
+        </div>
+        <div class="Satır" style="margin-top: 25px">
+            <asp:LinkButton ID="ltbn_kayıt" runat="server" Text="Kayıt Ol" CssClass="KayıtButon" OnClick="ltbn_kayıt_Click"></asp:LinkButton>
+        </div>
+        <div>
+            <asp:Label ID="lbl_mesaj" runat="server" CssClass="HataMesajı" Visible="false"></asp:Label>
+        </div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ KayıtFormuOdev/ASP/KayıtFormu.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KayıtFormuOdev.ASP_Kontrolleri
+{
+    public partial class KayıtFormu : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ltbn_kayıt_Click(object sender, EventArgs e)
+        {
+            bool KayıtBaşarılı = true;
+            if (!string.IsNullOrEmpty(tb_kullanıcı.Text))
+            {
+                if ((tb_kullanıcı.Text.Length) <= 25)
+                {
+                    if (!string.IsNullOrEmpty(tb_email.Text))
+                    {
+                        if (tb_email.Text.Length <= 150)
+                        {
+                            if (!string.IsNullOrEmpty(tb_sifre.Text))
+                            {
+                                if (tb_sifre.Text.Length <= 20)
+                                {
+                                    if (this.tb_email.Text.Contains('@') || this.tb_email.Text.Contains('.'))
+                                    {
+                                        if (KayıtBaşarılı)
+                                        {
+                                            KayıtBaşarılı = true;
+                                            lbl_mesaj.Visible = true;
+                                            lbl_mesaj.Text = "Hoşgeldiniz";
+                                        }
+                                    }
+                                    else
+                                    {
+                                        KayıtBaşarılı = false;
+                                        lbl_mesaj.Visible = true;
+                                        lbl_mesaj.Text = "Lütfen geçerli bir e-mail giriniz.";
+                                    }
+                                }
+                                else
+                                {
+                                    KayıtBaşarılı = false;
+                                    lbl_mesaj.Visible = true;
+                                    lbl_mesaj.Text = "Şifre en fazla 20 karakterden oluşabilir.";
+                                }
+
+                            }
+                            else
+                            {
+                                KayıtBaşarılı = false;
+                                lbl_mesaj.Visible = true;
+                                lbl_mesaj.Text = "Şifre boş bırakılamaz.";
+                            }
+                        }
+                        else
+                        {
+                            KayıtBaşarılı = false;
+                            lbl_mesaj.Visible = true;
+                            lbl_mesaj.Text = "Lütfen geçerli bir email giriniz.";
+                        }
+                    }
+                    else
+                    {
+                        KayıtBaşarılı = false;
+                        lbl_mesaj.Visible = true;
+                        lbl_mesaj.Text = "Email boş bırakılamaz.";
+                    }
+                }
+                else
+                {
+                    KayıtBaşarılı = false;
+                    lbl_mesaj.Visible = true;
+                    lbl_mesaj.Text = "Kullanıcı adı en fazla 25 karakterden oluşabilir.";
+                }
+            }
+            else
+            {
+                KayıtBaşarılı = false;
+                lbl_mesaj.Visible = true;
+                lbl_mesaj.Text = "Kullanıcı adı boş bırakılamaz.";
+            }
+        }
+    }
+}
